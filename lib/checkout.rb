79ec422fa717ec6da04338f25938fde8190577d7
jamierose <jrose111284@gmail.com>
2016-01-30 15:38:55 +0000

    it works out promotions

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,11 +1,10 @@
 require 'json'
 class Checkout
 
-  def initialize(products_json)
+  def initialize(products_json, promotions_json)
     @products = JSON.parse(File.read(products_json))
+    @promotions = JSON.parse(File.read(promotions_json))
     @order = []
-    # require 'byebug'
-    # byebug
   end
 
   def scan(barcode)
@@ -14,13 +13,34 @@ class Checkout
     @order << barcode
   end
 
-
-
   def total
     total_price = 0.0
     @order.each do |product_code|
       total_price += @products[product_code]["product_price"]
     end
+    apply_total_price_promotion(total_price).round(2)
+  end
+  private
+
+  def apply_total_price_promotion(total_price)
+    correct_promotion = find_correct_total_price_promotion(total_price)
+    if correct_promotion
+      discount_percentage = @promotions["total_price_promotions"][correct_promotion.to_s]["discount_percentage"]
+      return total_price -= (total_price * discount_percentage)
+    end
     total_price
   end
+
+  def find_correct_total_price_promotion(total_price)
+    available_promotions = []
+    correct_promotion = false
+    @promotions["total_price_promotions"].each_key { |k| available_promotions << k.to_i }
+    available_promotions.sort.each { |x| correct_promotion = x if total_price > x}
+    correct_promotion
+  end
+
+  def apply_product_promotions
+
+  end
+
 end
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,7 +1,7 @@
 require "checkout"
 
 describe Checkout do
-  subject(:checkout) { described_class.new('./products.json') }
+  subject(:checkout) { described_class.new('./products.json', './promotions.json') }
 
   it "can scan a barcode" do
     checkout.scan("001")
@@ -16,11 +16,30 @@ describe Checkout do
     expect { checkout.scan(001) }.to raise_error "please enter string value"
   end
 
-
-  xit "under £60, applying no promotions" do
+  it "adds up prices" do
     checkout.scan('001')
     checkout.scan('002')
     expect(checkout.total).to eq (54.25)
+  end
+
+  it "applys over £60 promotion" do
+    checkout.scan('001')
+    checkout.scan('002')
+    checkout.scan('003')
+    expect(checkout.total).to eq (66.78)
+  end
+
+  it "applys over £60 promotion" do
+    checkout.scan('002')
+    checkout.scan('002')
+    checkout.scan('003')
+    expect(checkout.total).to eq (76.97)
+  end
+
+  it "applys discount when ordering over two 001 products" do
+    checkout.scan('001')
+    checkout.scan('001')
+    expect(checkout.total).to eq (17.00)
   end
 
 end

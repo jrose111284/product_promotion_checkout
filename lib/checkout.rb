2bacde06362054f0c8534906505320c3efec9dd2
jamierose <jrose111284@gmail.com>
2016-01-31 21:19:43 +0000

    discount when buying two or more lavender hearts

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -16,10 +16,15 @@ class Checkout
   def total
     total_price = 0.0
     @order.each do |product_code|
+    if product_on_promotion(product_code) && product_fulfulls_promotion(product_code)
+      total_price += @promotions["product_promotions"][product_code]["discount_price"]
+    else
       total_price += @products[product_code]["product_price"]
+    end
     end
     apply_total_price_promotion(total_price).round(2)
   end
+
   private
 
   def apply_total_price_promotion(total_price)
@@ -39,8 +44,13 @@ class Checkout
     correct_promotion
   end
 
-  def apply_product_promotions
+  def product_on_promotion(product_code)
+    @promotions["product_promotions"].has_key?(product_code)
+  end
 
+  def product_fulfulls_promotion(product_code)
+    required_amount = @promotions["product_promotions"][product_code]["quantity_required"]
+    @order.count(product_code) >= required_amount
   end
 
 end
